ec380fd87de343dd5c158542b9b612e2baca1bfd
roufhasan <roufhasan5@gmail.com>
2025-12-28 13:08:08 +0600

    fix: signup password validation improved

--- src/data/authFormValidationRules.js
+++ src/data/authFormValidationRules.js
@@ -3,13 +3,13 @@ export const authFormValidationRules = {
     minLength: {
       value: 13,
       message:
-        "Password must be at least 13 characters long and contain uppercase, lowercase, number and special character",
+        "Password must be at least 13 characters long and contain uppercase, lowercase, number and two special characters",
     },
     pattern: {
       value:
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*\/])[A-Za-z\d!@#$%^&*\/]{13,}$/,
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d].*[^A-Za-z\d]).{13,}$/,
       message:
-        "Password must contain uppercase, lowercase, number and special character (!@#$%^&*/)",
+        "Password must contain at least one uppercase, one lowercase, one number and two special characters",
     },
   },
 
